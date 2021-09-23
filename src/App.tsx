b1f5c5ff5e1e7a8c5c24f9c887d4d7f1760bf2f7
Megan Englert <megan.adare@live.com>
2021-09-23 19:09:31 -0400

    Found and cleaned jeopardy question data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import QUESTIONS from './assets/1000questions.json';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      
+      {JSON.stringify(QUESTIONS)};
+
     </div>
   );
 }
---
+++ src/interfaces/question.ts
@@ -0,0 +1,12 @@
+export enum Round {
+    Roun
+}
+
+export interface Question {
+    ID: number
+    round: string
+    category: string
+    value: string
+    Prompt: string
+    Answer: string
+}
