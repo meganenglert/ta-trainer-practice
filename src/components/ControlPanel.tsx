953d49860d242d65ab3cdff2f954119cd0b7219d
Megan Englert <megan.adare@live.com>
2021-09-23 21:48:38 -0400

    Button changes the question!! Whoop!

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,11 +1,18 @@
 import { Button , Col} from 'react-bootstrap';
 import { Question } from '../interfaces/question';
+import QUESTIONS from '../assets/1000questions.json';
 
+// Same source as Dr. Bart used (I am lazy)
+function getRandomElement<T>(items: T[]): T  {
+    return items[Math.floor(Math.random()*items.length)];
+}
 
 export function ControlPanel({setQuestion}: {setQuestion: (q: Question)=>void}): JSX.Element {
     return <Col>
         <h1>Control Panel</h1>
         {setQuestion}
-        <Button>New Question</Button>
+        <Button onClick={
+            () => setQuestion(getRandomElement(QUESTIONS) as Question)
+        }>New Question</Button>
     </Col>
 }
