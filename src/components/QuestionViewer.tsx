b412c257efd3e721f259de4945c3408f32de2276
Megan Englert <megan.adare@live.com>
2021-09-23 22:00:32 -0400

    Card formatting for question/answer

--- src/components/QuestionViewer.tsx
+++ src/components/QuestionViewer.tsx
@@ -1,10 +1,25 @@
-import {Col} from 'react-bootstrap';
+import { Col, Card } from 'react-bootstrap';
 import { Question } from '../interfaces/question';
 
-export function QuestionViewer({question} : {question: Question}): JSX.Element {
+export function QuestionViewer({ question }: { question: Question }): JSX.Element {
     return <Col>
         <h1>Question Viewer</h1>
-        <div>{question.prompt}</div>
-        <div>Answer: {question.answer}</div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>Question:</Card.Title>
+                <Card.Text>
+                    {question.prompt}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>Answer:</Card.Title>
+                <Card.Text>
+                    {question.answer}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     </Col>
 }
