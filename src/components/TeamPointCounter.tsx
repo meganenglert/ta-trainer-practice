14924c5348b52dcd62b38073d876e76ddb4fb72c
Megan Englert <megan.adare@live.com>
2021-09-30 13:28:57 -0400

    Dummy add team name input appears with button No functionality yet

--- src/components/TeamPointCounter.tsx
+++ src/components/TeamPointCounter.tsx
@@ -1,10 +1,12 @@
-import { Col, Card, ButtonGroup, Button, Row } from 'react-bootstrap';
+import { Col, Card, ButtonGroup, Button, Row, InputGroup, FormControl } from 'react-bootstrap';
 import { Question } from '../interfaces/question';
 import { Team } from '../interfaces/team';
 
-export function TeamPointCounter({teamList, points, addPoints, addTeamRevealed, switchAddTeam }:
-    { teamList: Team[], points: number, addPoints: (t: Team, p: number) => void,
-        addTeamRevealed: boolean, switchAddTeam: ()=>void }): JSX.Element {
+export function TeamPointCounter({ teamList, points, addPoints, addTeamRevealed, switchAddTeam }:
+    {
+        teamList: Team[], points: number, addPoints: (t: Team, p: number) => void,
+        addTeamRevealed: boolean, switchAddTeam: () => void
+    }): JSX.Element {
 
     return <div id="scores">
         <h1>Scoreboard</h1>
@@ -29,13 +31,18 @@ export function TeamPointCounter({teamList, points, addPoints, addTeamRevealed, 
         </Row>
         <Button onClick={switchAddTeam} id="add-team">+ Add Team</Button>
         {addTeamRevealed && <Card.Body>
-                <Card.Text>
-                    woohoo
-                </Card.Text>
-            </Card.Body>}
+            <InputGroup className="mb-3">
+                <FormControl
+                    type="string" placeholder="New Team Name"
+                />
+                <Button onClick={addNewTeam} variant="outline-secondary" id="add-new-team">
+                    Add!
+                </Button>
+            </InputGroup>
+        </Card.Body>}
     </div >
 
-    
+
 
 }
 
